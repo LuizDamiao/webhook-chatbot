bf646efaa9ff88c1ca019f90ebbb38e7b9e4da5b
Luiz Gustavo Ferreira Damiao <85051560+LuizDamiao@users.noreply.github.com>
2026-06-05 23:30:15 -0300

    Fix CORS: add PUT and DELETE to allowed methods
    - Templates save (PUT) was blocked by CORS preflight
    - Added PUT and DELETE to Access-Control-Allow-Methods
    - All 73 tests passing

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ function nowBrasilia() {
 // CORS headers for GitHub Pages
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
